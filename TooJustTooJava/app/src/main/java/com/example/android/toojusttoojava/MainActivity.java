7ea29f0a24772ac93e205638f50c9617bed63150
joe-BlanketTheThird <popeofpoetry@yahoo.com>
2016-06-26 17:10:12 -0600

    Added new method from lesson. Also fixed unintentional loop in decrement method. Changed variable usage and moved globals to top.

--- TooJustTooJava/app/src/main/java/com/example/android/toojusttoojava/MainActivity.java
+++ TooJustTooJava/app/src/main/java/com/example/android/toojusttoojava/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.toojusttoojava;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import java.text.NumberFormat;
@@ -9,7 +9,13 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * These global variables store the quantity for an order and price per unit.
+     */
+    int quantity = 0;
+    double pricePerUnit = 2.50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,37 +23,43 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 0;
-
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        double price = quantity * 2.50;
-        String priceMessage = "Your Price: $" + price;
+        String priceMessage = "Your Price: $" + calculatePrice(quantity);
 	    priceMessage = priceMessage + "\n Thank You!";
         displayMessage(priceMessage);
     }
+
+    /**
+     * Calculates the price of the order based on the current quantity and price per unit.
+     * TODO: Currently returns a double, will need to be adjusted later.
+     * */
+    private double calculatePrice(int quantity) {
+        return quantity * pricePerUnit;
+    }
+
     /**
      * The following methods increment or decrement the number of coffees.
+     * Decrement will not allow the order quantity to go below 0.
      */
     public void increment(View view) {
         quantity ++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         if (quantity > 0){
             quantity --;
-            display(quantity);
         }
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
